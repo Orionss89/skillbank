506c2b2fe04a2cdc61392e72b84b86d888c2f803
Lukasz Ura <bravura71@gmail.com>
2025-11-24 19:31:48 +0100

    Initial commit

---
+++ src/main/java/com/skillbank/SkillbankApplication.java
@@ -0,0 +1,13 @@
+package com.skillbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillbank/SkillbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
