a65bbfedd7654d30205828fbdaa4fd19914b353b
Lukasz Ura <bravura71@gmail.com>
2025-11-25 09:02:10 +0100

    Security

--- src/main/java/com/skillbank/config/SecurityConfig.java
+++ src/main/java/com/skillbank/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // To klasa konfiguracyjna
+@Configuration
 public class SecurityConfig {
 
     @Bean
