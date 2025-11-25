5b1319acc829fd3be88cc5f0bd4fe6e85a2f6fdb
Lukasz Ura <bravura71@gmail.com>
2025-11-25 23:11:00 +0100

    Dodanie interfejsów Repozytoriów dla wszystkich encji

---
+++ src/main/java/com/skillbank/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.skillbank.repository;
+
+import com.skillbank.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
