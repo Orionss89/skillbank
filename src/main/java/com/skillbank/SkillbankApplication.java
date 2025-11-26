c709fbd1f1d424c8c594ea5ecda1e51c521950c9
Lukasz Ura <bravura71@gmail.com>
2025-11-26 21:01:28 +0100

    Finalizacja

--- src/main/java/com/skillbank/SkillbankApplication.java
+++ src/main/java/com/skillbank/SkillbankApplication.java
@@ -2,6 +2,8 @@ package com.skillbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SkillbankApplication {
@@ -9,5 +11,8 @@ public class SkillbankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SkillbankApplication.class, args);
 	}
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
